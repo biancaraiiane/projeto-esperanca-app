feddb67092bed99a2d5004466b1c76c1d8f7ce9f
Bruno Alexandre Andrade Martins <alexandrebruno110705@gmail.com>
2026-05-25 20:03:29 -0300

    feat: :sparkles: create component carousel component detailsProjects and projectsPage

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,14 +1,12 @@
 ﻿
 import { LandingHero } from "@/components/LandingHero";
 import { QuemSomos } from "@/components/QuemSomos";
-import { TransparencySection } from "@/components/TransparencySection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-(--bg-main)">
       <LandingHero/>
       <QuemSomos />
-      <TransparencySection />
     </main>
   );
 }
