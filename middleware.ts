c2d85d961144630a984a0788ed919cd0630ad634
Bianca <bianca.raiane18@gmail.com>
2026-05-28 02:59:06 -0300

    feat: :saprkles: create private route config and insert the singOut function from sidebar component

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const COOKIE_NAME = "@ESPERANÇA:T";
+const COOKIE_NAME = "@ESPERANCA:T";
 const LOGIN_PAGE = "/login";
 const DEFAULT_ADMIN_PAGE = "/inicio";
 
